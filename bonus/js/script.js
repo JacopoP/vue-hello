07d6f0bd0bcb16400052db6fb73d04d2a7374761
Jac22 <jacopo.prudentino@gmail.com>
2022-11-25 13:01:48 +0100

    Provato methods in vue

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -7,5 +7,10 @@ createApp ({
             img : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1024px-Vue.js_Logo_2.svg.png",
             altImg : "logo Vue js"
         }
+    },
+    methods: {
+        changeColor : function(){
+            event.target.style.color="red";
+        },
     }
 }).mount("#root");
